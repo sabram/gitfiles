2a40e4bf96efa0751e1f2f744d1fc84b76e18636
sabram <sabram@lendinglcub.com>
2017-04-13 21:57:55 -0700

    Added git.sh

---
+++ git.sh
@@ -0,0 +1,20 @@
+#Git shortcuts. Source from your bash profile.
+
+source ~/git-completion.bash
+
+alias glog='git log origin/development..development'
+alias glog2='git log origin/develop..develop'
+alias gdiff='git diff origin/development..development'
+alias gdiff2='git diff origin/develop..develop'
+alias vigc="vi ~/.gitconfig"
+alias catgc="cat /Users/sabram/dev/gitfiles/.gitconfig"
+alias s='git status'
+
+# add git branch to prompt
+# http://codeinthehole.com/writing/pull-requests-and-other-good-practices-for-teams-using-github/
+function parse_git_branch {
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+}
+#Note that the 94m here is Light blue for command prompt after the branch name.
+PS1="\[\e[32m\]\$(parse_git_branch)\[\e[94m\]\W \$ \[\e[m\]"
+export PS1
